c17253289f0de5ac49efaeba6dddd00c27f20f21
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-08-23 15:15:31 -0700

    fix routes that was giving errors for form_with

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
+  resources :posts, only: [:new, :create, :index]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
