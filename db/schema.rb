44ec793bfffd4a2255c03ad29d1aac0159a45e2f
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-08-22 23:37:16 -0700

    add then remove migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_054337) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_063619) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
