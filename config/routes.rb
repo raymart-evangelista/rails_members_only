54548e1ead3467bec7aefff389d91a6638155bf2
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-08-22 22:55:33 -0700

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "posts#index"
 end
